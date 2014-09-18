edb8ff87fcaa79ae75210901e81a55f5c03f4815
BennyHallett <benny.hallett@gmail.com>
2014-09-19 08:29:43 +1000

    Changed A-high straight flush to A-low

--- test/ai_test.exs
+++ test/ai_test.exs
@@ -18,20 +18,20 @@ defmodule AiTest do
     ] ] == Poker.Ai.discard hand
   end
 
-  test "AI keeps a straight flush ace hight" do
+  test "AI keeps a straight flush ace low" do
     hand = [
-      %Poker.Card{ value: "J", suit: :hearts },
-      %Poker.Card{ value: 10, suit: :hearts },
+      %Poker.Card{ value: 5, suit: :hearts },
+      %Poker.Card{ value: 3, suit: :hearts },
       %Poker.Card{ value: "A", suit: :hearts },
-      %Poker.Card{ value: "Q", suit: :hearts },
-      %Poker.Card{ value: "K", suit: :hearts }
+      %Poker.Card{ value: 4, suit: :hearts },
+      %Poker.Card{ value: 2, suit: :hearts }
     ]
     assert [ [], [
       %Poker.Card{ value: "A", suit: :hearts },
-      %Poker.Card{ value: "K", suit: :hearts },
-      %Poker.Card{ value: "Q", suit: :hearts },
-      %Poker.Card{ value: "J", suit: :hearts },
-      %Poker.Card{ value: 10, suit: :hearts }
+      %Poker.Card{ value: 5, suit: :hearts },
+      %Poker.Card{ value: 4, suit: :hearts },
+      %Poker.Card{ value: 3, suit: :hearts },
+      %Poker.Card{ value: 2, suit: :hearts }
     ] ] == Poker.Ai.discard hand
   end
 
